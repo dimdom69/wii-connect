12108917385a27e84f3379d16ebd1458e02a8a05
lehriana <lehriana@097a44a4-530b-11de-b1d8-e9bd45c23c72>
2009-06-09 01:55:38 +0000

    E-mail added

---
+++ source/email.h
@@ -0,0 +1,29 @@
+class email{
+	
+
+public:
+	
+	email();
+	~email();
+	void send(msg);
+	
+	
+	struc msg{
+		char *from;
+		char *to;
+		char *subject;
+		char *message;
+		
+	}
+	
+protected:	
+
+private:
+	
+	
+	
+	
+	
+	
+	
+}
