6ec1e09baca8dbcd7cd3227beb110c3c2ca7e6a9
lehriana <lehriana@097a44a4-530b-11de-b1d8-e9bd45c23c72>
2009-07-27 05:26:14 +0000

    Lot's of work on the stamp sheet parser. Almost done!

--- source/stamp.h
+++ source/stamp.h
@@ -23,21 +23,68 @@
 #ifndef STAMP_H
 #define STAMP_H
 
+#include <vector>
+
+using namespace std;
+
+typedef struct stampc_s{
+	char *console;
+	char *subconsole;
+	char *genre;
+	char *rating;
+	char *developer;
+	char *publisher;
+}stampc;
+
+
+typedef struct stamph_s{
+	char *title;
+	char *gameid;
+	char *creator;
+	int num;
+}stamph;
+
+typedef struct stampf_s{
+	char *info;
+	stampc *category;
+}stampf;
+
+typedef struct stampa_s{
+	int num;
+	char *title;
+	int points;
+	char *offset;
+	char *value;
+}stampa;
+
+
+typedef struct stampsheet_s{
+	stamph *header;
+	vector<stampa*> stamps;
+	stampf *footer;
+}stampsheet;
+
 class stamp
 {
 	public:
-		stamp(char *file);
+		//dont worry about first, it is only used internally by the class
+		stamp(char *f);
 		virtual ~stamp();
-		void loadstamp();
-		bool checkstamp();
+		void loadstamps();
+		void loadstamp(char *snum);
 		void deletestamp();
-		bool cont();
-		
-		stamp *next;
+		char *tonum(int n);
+		void loadheader();
+		void loadfooter();
 		
 	
 	private:
-		
+		stampsheet *ss;
+		int length;
+		int pos;
+		char *tmp;
+		char *file;
+		stampa *current;
 };
 
 #endif /* STAMP_H */ 
