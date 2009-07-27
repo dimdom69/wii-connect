5254a7a46f1ed048beb9a316557dd4954d1b028e
lehriana <lehriana@097a44a4-530b-11de-b1d8-e9bd45c23c72>
2009-07-27 05:26:14 +0000

    Lot's of work on the stamp sheet parser. Almost done!

--- trunk/source/stamp.cpp
+++ trunk/source/stamp.cpp
@@ -21,11 +21,15 @@
 
 
 #include "stamp.h"
+#include <string.h>
 
 
-stamp::stamp(char *file)
+
+stamp::stamp(char *f)
 {
-	
+	file = f;
+	ss = new stampsheet;
+	pos = 0;
 }
 
 
@@ -34,27 +38,111 @@ stamp::~stamp()
 	
 }
 
+char *stamp::tonum(int n){
+	tmp = new char [3+1];
+	if(n<10){
+		sprintf(tmp,"00%d",n);
+	}
+	else if(n<100){
+		sprintf(tmp,"0%d",n);
+	}
+	else{
+		sprintf(tmp,"%d",n);
+	}
+	return tmp;
+}	
+		
+		
+void stamp::loadheader()
+{
+	ss->header = new stamph;
+	if((tmp=strstr(file,"Title"))){
+		pos = tmp-file;
+		pos += strlen("Title");
+		tmp = strchr(file+pos, '=');
+		pos = tmp-file;
+		pos++;
+		while((int)file[pos] < 33 && file[pos]) pos++;
+		tmp = strchr(file+pos,'-');
+		length = tmp-(file+pos);
+		ss->header->title = new char [length+1];
+		strncpy(ss->header->title,tmp,length);
+		ss->header->title[length] = '\0';
+	}	
+}
 
-
-void stamp::loadstamp()
+void stamp::loadfooter()
 {
 	
 }
-
-bool stamp::checkstamp()
+		
+void stamp::loadstamps()
 {
-	
+	for(int x = 0;x<ss->header->num;x++)
+	{
+		loadstamp(tonum(x+1));
+	}
 }
 
+void stamp::loadstamp(char *snum)
+{
+	current = new stampa;
+	current->num = atoi(snum);
+	tmp = strstr(file, snum);
+	if(!tmp) return;
+	pos = tmp-file;
+	pos += 3;
+	tmp = strchr(file+pos,'=');
+	pos = tmp-file;
+	pos++;
+	while((int)file[pos] < 33 && file[pos]) pos++;
+	tmp = strchr(file+pos,',');
+	length = tmp-(file+pos);
+	current->title = new char [length+1];
+	strncpy(current->title,file+pos,length);
+	current->title[length] = '\0';
+	pos = tmp-file;
+	pos++;
+	
+	
+	while((int)file[pos] < 33 && file[pos]) pos++;
+	tmp = strchr(file+pos,'p');
+	length = tmp-(file+pos);
+	tmp = new char [length+1];
+	strncpy(tmp,file+pos,length);
+	tmp[length] = '\0';
+	current->points = atoi(tmp);
+	delete [] tmp;
+	
+	
+	tmp = strchr(file+pos,',');
+	pos++;
+	while((int)file[pos] < 33 && file[pos]) pos++;
+	tmp = strchr(file+pos,',');
+	length = tmp-(file+pos);
+	tmp = new char [length+1];
+	strncpy(tmp,file+pos,length);
+	tmp[length] = '\0';
+	current->offset = (char*)atoi(tmp);
+	delete [] tmp;
+	
+	
+	
+	tmp = strchr(file+pos,',');
+	pos++;
+	while((int)file[pos] < 33 && file[pos]) pos++;
+	tmp = strchr(file+pos,',');
+	length = tmp-(file+pos);
+	tmp = new char [length+1];
+	strncpy(tmp,file+pos,length);
+	tmp[length] = '\0';
+	current->value = (char*)atoi(tmp);
+	delete [] tmp;
+	
+	ss->stamps.push_back(current);
+}
 
 void stamp::deletestamp()
 {
 	
 }
-
-
-bool stamp::cont()
-{
-	
-}
-
